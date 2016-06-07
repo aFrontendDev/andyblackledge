cb35ae2ecfba106a21bcbbfe28fee8ce3c5cf525
ablackledge <a.blackledge@building-blocks.com>
2016-06-07 17:02:50 +0100

    adding some initial styles and updating gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pxtorem = require('gulp-pxtorem');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
     return gulp.src('./sass/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+     .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(pxtorem())
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./sass/**/*.scss', ['default']);
 });
